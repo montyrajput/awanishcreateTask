402525cabc7731052b15d2e3001ec4d64c57d3c3
montyrajput <montysinghrahput590@gmail.com>
2023-07-07 15:59:38 +0000

    soution

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import CreateTask from './components/createTask'
+
+import './App.css'
+
+// These are the lists used in the application. You can move them to any component needed.
+
+// Replace your code here
+const App = () => <CreateTask />
+
+export default App
